76e267f673d086b7bb35f22a9ebacb6730d2e108
aylingyoren <aylingyoren21@gmail.com>
2022-01-27 13:38:18 +0300

    add component binding

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from "yup";
 import CardView from '../CardView';
@@ -15,11 +16,17 @@ type CardProps = {
 }
 
 function Card({isDarkModeActive}: CardProps) {
+  const [cardNumber, setCardNumber] = useState('');
+  const [cardHolder, setCardHolder] = useState('');
+  const [CVV, setCVV] = useState('');
+  const [month, setMonth] = useState('');
+  const [year, setYear] = useState('');
+
   const initialValues: MyFormValues = { cardNumber: '', cardHolder: '', CVV: '', expDate: '' };
   return (
     <div>
         <h2 className={isDarkModeActive ? "header light-text" : "header"}>Visa Card</h2>
-        <CardView />
+        <CardView cardNumber={cardNumber} cardHolder={cardHolder} CVV={CVV} month={month} year={year} />
         <div className={isDarkModeActive ? "card-validator light-border" : "card-validator"}>
           <Formik
             initialValues={initialValues}
@@ -39,7 +46,11 @@ function Card({isDarkModeActive}: CardProps) {
               expDate: Yup.string()
                 .label('Expiration Date')
                 .required('Required'),
-              CVV: Yup.string().label('CVV Number').min(3).max(4).required('Required'),
+              CVV: Yup.string()
+                .label('CVV Number')
+                .min(3, 'Must be 3 or 4 digits')
+                .max(4, 'Must be 3 or 4 digits')
+                .required('Required'),
             })}
             onSubmit={(values, {setSubmitting}) => {
               setTimeout(() => {
@@ -51,41 +62,41 @@ function Card({isDarkModeActive}: CardProps) {
             {({ isSubmitting }) => (
               <Form className='card-fields'>
                 <label className={isDarkModeActive ? 'card-field-label light-text' : 'card-field-label dark-text'} htmlFor="cardNumber">Card Number</label>
-                <Field className='card-field' name="cardNumber" type="text" />
+                <Field className='card-field' name="cardNumber" type="text" value={cardNumber} onChange={(e: any) => setCardNumber(e.target.value)} />
                 <span className={isDarkModeActive ? 'err-msg light-err' : 'err-msg'}><ErrorMessage name="cardNumber" /></span>
               
                 <label className={isDarkModeActive ? 'card-field-label light-text' : 'card-field-label dark-text'} htmlFor="cardHolder">Card Holder</label>
-                <Field className='card-field' name="cardHolder" type="text" />
+                <Field className='card-field' name="cardHolder" type="text" value={cardHolder} onChange={(e: any) => setCardHolder(e.target.value)} />
                 <span className={isDarkModeActive ? 'err-msg light-err' : 'err-msg'}><ErrorMessage name="cardHolder" /></span>
 
                 <div className="expDate-cvv">
                   <div className="expDate-set">
                     <label className={isDarkModeActive ? 'card-field-label light-text' : 'card-field-label dark-text'} htmlFor="month">Expiration Date</label>
                     <div className="expDate-field">
-                      <Field className='card-field expDate-input' as="select" name="expDate">
+                      <Field className='card-field expDate-input' as="select" name="expDate" value={month} onChange={(e: any) => setMonth(e.target.value)}>
                         <option value="month" selected>Month</option>
-                        <option value="january">January</option>
-                        <option value="february">February</option>
-                        <option value="march">March</option>
-                        <option value="april">April</option>
-                        <option value="may">May</option>
-                        <option value="june">June</option>
-                        <option value="july">July</option>
-                        <option value="august">August</option>
-                        <option value="september">September</option>
-                        <option value="october">October</option>
-                        <option value="november">November</option>
-                        <option value="december">December</option>
+                        <option value="01">January</option>
+                        <option value="02">February</option>
+                        <option value="03">March</option>
+                        <option value="04">April</option>
+                        <option value="05">May</option>
+                        <option value="06">June</option>
+                        <option value="07">July</option>
+                        <option value="08">August</option>
+                        <option value="09">September</option>
+                        <option value="10">October</option>
+                        <option value="11">November</option>
+                        <option value="12">December</option>
                       </Field>
 
-                      <Field className='card-field expDate-input' as="select" name="expDate">
+                      <Field className='card-field expDate-input' as="select" name="expDate" value={year} onChange={(e: any) => setYear(e.target.value)}>
                         <option value="year" selected>Year</option>
-                        <option value="2027">2027</option>
-                        <option value="2026">2026</option>
-                        <option value="2025">2025</option>
-                        <option value="2024">2024</option>
-                        <option value="2023">2023</option>
-                        <option value="2022">2022</option>
+                        <option value="27">2027</option>
+                        <option value="26">2026</option>
+                        <option value="25">2025</option>
+                        <option value="24">2024</option>
+                        <option value="23">2023</option>
+                        <option value="22">2022</option>
                       </Field>
                     </div>
                     
@@ -94,7 +105,7 @@ function Card({isDarkModeActive}: CardProps) {
                 
                   <div className="cvv-set">
                     <label className={isDarkModeActive ? 'card-field-label light-text' : 'card-field-label dark-text'} htmlFor="CVV">CVV</label>
-                    <Field className='card-field cvv-input' name="CVV" type="text" />
+                    <Field className='card-field cvv-input' name="CVV" type="text" value={CVV} onChange={(e: any) => setCVV(e.target.value)} />
                     <span className={isDarkModeActive ? 'err-msg light-err' : 'err-msg'}><ErrorMessage name="CVV" /></span>
                   </div>
                 </div>
--- src/components/CardView/CardView.tsx
+++ src/components/CardView/CardView.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './CardView.css';
 
-function CardView() {
+type CardViewProps = {
+  cardNumber: string;
+  cardHolder: string;
+  CVV: string;
+  month: string;
+  year: string;
+}
+
+function CardView({cardNumber, cardHolder, CVV, month, year}: CardViewProps) {
   return (
     <div className="card-body">
-      <h2 className="card-number">0000 0000 0000 0000</h2>
-      <div className="card-holder">NAME SURNAME</div>
-      <div className="exp-date">00 / 00</div>
+      <h2 className="card-number">{cardNumber? `${cardNumber.slice(0,4)} ${cardNumber.slice(4,8)} ${cardNumber.slice(8,12)} ${cardNumber.slice(12,16)}`  : '0000 0000 0000 0000'}</h2>
+      <div className="card-holder">{cardHolder ? cardHolder :'NAME SURNAME'}</div>
+      <div className="exp-date">{month ? month  : '00'} / {year ? year :'00'}</div>
+      <div className="cvv">CVV: {CVV ? CVV : '000(0)'}</div>
       <img src={require("../../img/chip-card.png")} alt="card chip" className="chip-img" />
       <img src={require("../../img/visa.png")} alt="visa logo" className="visa-img" />
     </div>
